d30b0f1614f1f8a7cda8fc67577055c4f0eb048e
neo321654 <neo321654@rambler.ru>
2021-11-08 23:41:04 +0300

    убрал ошибку

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ import 'src/settings/settings_service.dart';
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
+  //строка ниже помогает убрать ошибку подгрузки
+  //todo не сохраняются новые даты в настройках
+  WidgetsFlutterBinding.ensureInitialized();
   final settingsController = SettingsController(SettingsService());
 
   // Load the user's preferred theme while the splash screen is displayed.
